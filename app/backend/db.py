17a46a476ce3b1c17bf688661812ee65ef4f8ce8
Fizikaman <fizikaman26@gmail.com>
2024-08-25 14:34:33 +0300

    переход на постгрес

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,9 +1,10 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
 
 
-engine = create_engine('sqlite:///fizik_store.db', echo=True)
-SessionLocal = sessionmaker(bind=engine)
+engine = create_async_engine('postgresql+asyncpg://mis_user:password@localhost:54324/fast_api_store',
+                             echo=True)
+async_session_marker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
 class Base(DeclarativeBase):
--- app/backend/db_depends.py
+++ app/backend/db_depends.py
@@ -1,9 +1,8 @@
-from app.backend.db import SessionLocal
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.backend.db import async_session_marker
 
 
-async def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+async def get_db() -> AsyncSession:
+    async with async_session_marker() as session:
+        yield session
