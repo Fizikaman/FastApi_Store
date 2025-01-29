9c0afb630655bea7dc8cdc4da3009bc1522a418b
Fizikaman <fizikaman26@gmail.com>
2025-01-29 23:03:01 +0300

    delete_user url

--- app/routers/permission.py
+++ app/routers/permission.py
@@ -16,6 +16,7 @@ router = APIRouter(prefix='/permission', tags=['permission'])
 async def supplier_permission(db: Annotated[AsyncSession, Depends(get_db)],
                               get_user: Annotated[dict, Depends(get_current_user)],
                               user_id: int):
+    """Точка для смены роли у юзера"""
     if get_user.get('is_admin'):
         user = await db.scalar(select(User).where(User.id == user_id))
 
@@ -37,6 +38,45 @@ async def supplier_permission(db: Annotated[AsyncSession, Depends(get_db)],
             return {
                 'status_code': status.HTTP_200_OK,
                 'detail': 'User is now supplier'
+            }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You don't have admin permission"
+        )
+
+
+@router.delete('/delete')
+async def delete_user(db: Annotated[AsyncSession, Depends(get_db)],
+                      get_user: Annotated[dict, Depends(get_current_user)],
+                      user_id: int):
+    """Точка для удаления пользователей"""
+    if get_user.get('is_admin'):
+        user = await db.scalar(select(User).where(User.id == user_id))
+
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='User not found'
+            )
+
+        if user.is_admin:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You can't delete admin user"
+            )
+
+        if user.is_active:
+            await db.execute(update(User).where(User.id == user_id).values(is_active=False))
+            await db.commit()
+            return {
+                'status_code': status.HTTP_200_OK,
+                'detail': 'User is deleted'
+            }
+        else:
+            return {
+                'status_code': status.HTTP_200_OK,
+                'detail': 'User has already been deleted'
             }
     else:
         raise HTTPException(
