63fddeff2c683854c6cea9185d12dab4b33e1944
Fizikaman <fizikaman26@gmail.com>
2025-01-31 13:44:48 +0300

    create and update products routers

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 from app.backend.db_depends import get_db
 from typing import Annotated
 
 from app.models import *
 from sqlalchemy import insert, select, update
+
+from app.routers.auth import get_current_user
 from app.schema import CreateCategory, CreateProduct
 
 from slugify import slugify
@@ -20,23 +23,35 @@ async def all_products(db: Annotated[Session, Depends(get_db)]):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='No products found')
 
 
-@router.post('/create')
-async def create_product(db: Annotated[Session, Depends(get_db)], create_product: CreateProduct) -> dict:
-    await db.execute(insert(Product).values(
-        name=create_product.name,
-        slug=slugify(create_product.name),
-        description=create_product.description,
-        price=create_product.price,
-        stock=create_product.stock,
-        image_url=create_product.image_url,
-        rating=0.0,
-        category_id=create_product.category
-    ))
-    await db.commit()
-    return {
-        'status_code': status.HTTP_201_CREATED,
-        'transaction': 'Successful'
-    }
+@router.post('/')
+async def create_product(db: Annotated[AsyncSession, Depends(get_db)], create_product: CreateProduct,
+                         get_user: Annotated[dict, Depends(get_current_user)]):
+    if get_user.get('is_supplier') or get_user.get('is_admin'):
+        category = await db.scalar(select(Category).where(Category.id == create_product.category))
+        if category is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='There is no category found'
+            )
+        await db.execute(insert(Product).values(name=create_product.name,
+                                                description=create_product.description,
+                                                price=create_product.price,
+                                                image_url=create_product.image_url,
+                                                stock=create_product.stock,
+                                                category_id=create_product.category,
+                                                rating=0.0,
+                                                slug=slugify(create_product.name),
+                                                supplier_id=get_user.get('id')))
+        await db.commit()
+        return {
+            'status_code': status.HTTP_201_CREATED,
+            'transaction': 'Successful'
+        }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='You have not enough permission for this action'
+        )
 
 
 
@@ -70,26 +85,46 @@ async def product_detail(db: Annotated[Session, Depends(get_db)], product_slug: 
 
 
 @router.put('/detail/{product_slug}')
-async def update_product(db: Annotated[Session, Depends(get_db)],
-                         update_product: CreateProduct, product_slug: str) -> dict:
-    product = await db.scalar(select(Category).where(Product.slug == product_slug))
-    if product is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Product not found')
+async def update_product(db: Annotated[AsyncSession, Depends(get_db)], product_slug: str,
+                         update_product_model: CreateProduct, get_user: Annotated[dict, Depends(get_current_user)]):
+    if get_user.get('is_supplier') or get_user.get('is_admin'):
+        product_update = await db.scalar(select(Product).where(Product.slug == product_slug))
+        if product_update is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='There is no product found'
+            )
+        if get_user.get('id') == product_update.supplier_id or get_user.get('is_admin'):
+            category = await db.scalar(select(Category).where(Category.id == update_product_model.category))
+            if category is None:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail='There is no category found'
+                )
+            product_update.name = update_product_model.name
+            product_update.description = update_product_model.description
+            product_update.price = update_product_model.price
+            product_update.image_url = update_product_model.image_url
+            product_update.stock = update_product_model.stock
+            product_update.category_id = update_product_model.category
+            product_update.slug = slugify(update_product_model.name)
 
-    await db.execute(update(Product).where(Product.slug == product_slug).values(
-        name=update_product.name,
-        slug=slugify(update_product.name),
-        description=update_product.description,
-        price=update_product.price,
-        stock=update_product.stock,
-        image_url=update_product.image_url,
-        category_id=update_product.category
-    ))
-    await db.commit()
-    return {
-        'status_code': status.HTTP_200_OK,
-        'transaction': 'Product updated'
-    }
+            await db.commit()
+            return {
+                'status_code': status.HTTP_200_OK,
+                'transaction': 'Product update is successful'
+            }
+        else:
+
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail='You have not enough permission for this action'
+            )
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='You have not enough permission for this action'
+        )
 
 
 @router.delete('/delete')
